28aacf8672cc198c6ec68d16109c643cb4abdd2a
Jeongeun-Choi <cje3080@gmail.com>
2020-10-19 22:55:09 +0900

    [Update] 클래스명 수정

--- components/locationSetting/Selection.jsx
+++ components/locationSetting/Selection.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { Divider } from 'antd';
-import { Modal, ModalHeader, ModalFooter } from '../../public/style';
+import { Modal, ModalHeader, modalFooter } from '../../public/style';
 import { LeftOutlined, RightOutlined } from '@ant-design/icons';
 
 const SelectionContainer = styled.form`
@@ -9,9 +9,15 @@ const SelectionContainer = styled.form`
   flex-direction: column;
   width: 100%;
   height: 100%;
+
+  .setting {
+    width: 90%;
+    height: 90px;
+  }
 `;
 
-const Footer = styled(ModalFooter)`
+const Footer = styled.button`
+  ${modalFooter}
   color: #ffffff;
   background-color: #cba6c3;
   border: 1px solid #cba6c3;
@@ -27,17 +33,16 @@ const Selection = () => {
           <h3>지역 및 관심분야 설정</h3>
         </ModalHeader>
         <Divider />
-        <section>
+        <section className="setting">
           <div className="section-header">
             <div>활동 선호 지역 설정 (0/3)</div>
             <RightOutlined />
           </div>
         </section>
-        
         <Divider />
-        <section>
+        <section className="setting">
           <div className="section-header">
-            <div>활동 선호 지역 설정 (0/3)</div>
+            <div>관심 분야 설정 (0/3)</div>
             <RightOutlined />
           </div>
         </section>
