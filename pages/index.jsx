a3d834d4733631fc50bd098b515f5c2005c929ff
Jeongeun-Choi <cje3080@gmail.com>
2020-10-19 22:54:46 +0900

    [Fix]Modal 형식 component 확인

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import InitialScreen from '../components/locationSetting/InitialScreen';
 import Selection from '../components/locationSetting/Selection';
+import LocationSetting from '../components/locationSetting/LocationSetting';
 
 const index = () => {
   return (
-    
-    <Selection />
+    <LocationSetting />
     // <>
     //   <h1>메인화면</h1>
     // </>
