f5c8f5c267448578b988c83f9a747bfa3ff178a7
Jeongeun-Choi <cje3080@gmail.com>
2020-10-21 00:48:41 +0900

    [Update] 메뉴바 수정

--- components/main/Footer.jsx
+++ components/main/Footer.jsx
@@ -1,21 +1,63 @@
-import React from 'react';
-import { Menubar, Menu } from '../../public/style';
+import React, { useRef, useState, useEffect } from 'react';
+import { Menubar } from '../../public/style';
 import Link from 'next/link';
+import {
+  HomeOutlined,
+  TeamOutlined,
+  HeartOutlined,
+  SearchOutlined,
+  EllipsisOutlined
+} from '@ant-design/icons';
 
-const Footer = () => {
+const Footer = ({ pathName }) => {
+  const menuRef = useRef();
+
+  const [selected, setSelected] = useState('main');
+
+  useEffect(() => {
+    const path = pathName.split('/')[1];
+    if (path === '') {
+      setSelected('main');
+    } else {
+      setSelected(path);
+    }
+  }, [pathName]);
+
   return (
-    <Menubar>
-      <Link href="/">
-        <Menu>1</Menu>
-      </Link>
-      <Link href="/search">
-        <Menu>2</Menu>
-      </Link>
-      <Menu>3</Menu>
-      <Link href="/team">
-        <Menu>4</Menu>
-      </Link>
-      <Menu>5</Menu>
+    <Menubar selected={selected} ref={menuRef}>
+      <ul>
+        <li>
+          <Link href="/">
+            <a name="main">
+              <HomeOutlined />
+            </a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/search">
+            <a name="search">
+              <SearchOutlined />
+            </a>
+          </Link>
+        </li>
+        <li>
+          <a name="like">
+            <HeartOutlined />
+          </a>
+        </li>
+        <li>
+          <Link href="/team">
+            <a name="team">
+              <TeamOutlined />
+            </a>
+          </Link>
+        </li>
+        <li>
+          <a name="mypage">
+            <EllipsisOutlined />
+          </a>
+        </li>
+      </ul>
     </Menubar>
   );
 };
