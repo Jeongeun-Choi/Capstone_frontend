a2208d29bef8b2d774a00b3e5b2e88ebc4314d82
Jeongeun-Choi <cje3080@gmail.com>
2020-10-25 03:11:30 +0900

    [Update] EmptyTeams component

--- components/team/EmptyTeams.jsx
+++ components/team/EmptyTeams.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import styled from '@emotion/styled';
+import { Modal } from 'antd';
+import ProvedComponent from './ProvedComponent';
+import MakingTeam from './MakingTeam';
 
 //어떻게 효율적으로 짤 수 있을까...
 const content = {
-  '나의 모임': userName => `원하는 모임이 없다면?만들어보세요!`,
-  '내가 지원한 모임': userName => `${userName}님과 잘 맞는 모임에 참여해보세요.`
+  '나의 모임': [`원하는 모임이 없다면?\n만들어보세요!`, '모임 만들기'],
+  '내가 지원한 모임': [`회원님과 잘 맞는 모임에 참여해보세요.`, '모임 참여하기']
 };
 
 const EmptyTeamsContainer = styled.main`
@@ -15,7 +18,10 @@ const EmptyTeamsContainer = styled.main`
   justify-content: center;
   align-items: center;
 
-  a {
+  & p {
+    text-align: center;
+  }
+  & button {
     width: 10%;
     min-width: 120px;
     border: 1px solid #e2e2e2;
@@ -23,12 +29,52 @@ const EmptyTeamsContainer = styled.main`
   }
 `;
 
-const EmptyTeams = ({ pageTab, userName }) => {
+const EmptyTeams = ({ pageTab }) => {
+  //임의로 실명인증이 안됐다고 설정함.
+  const isProved = false;
+  const [pText, aText] = content[pageTab];
+  const [isAlertModal, setIsAlertModal] = useState(false);
+  const [isProvedModal, setIsProvedModal] = useState(false);
+  const [showingMakingTeamModal, setShowingMakingTeamModal] = useState(false);
+
+  const openScreen = useCallback(() => {
+    if (!isProved) {
+      setIsProvedModal(prev => !prev);
+    } else {
+      setShowingMakingTeamModal(prev => !prev);
+    }
+    setIsAlertModal(prev => !prev);
+  }, [isProved]);
+
+  const openAlertModal = useCallback(() => {
+    setIsAlertModal(prev => !prev);
+  }, []);
+
   return (
-    <EmptyTeamsContainer>
-      <p>{content[pageTab](userName)}</p>
-      <a>모임 찾아보기</a>
-    </EmptyTeamsContainer>
+    <>
+      <EmptyTeamsContainer>
+        <p>
+          {pText.split('\n').map(text => (
+            <span key={text}>
+              {text}
+              <br />
+            </span>
+          ))}
+        </p>
+        <button onClick={openAlertModal}>{aText}</button>
+      </EmptyTeamsContainer>
+      <Modal visible={isAlertModal} onOk={openScreen} onClose={openAlertModal}>
+        <h4>모임 개설</h4>
+        <div>새로운 모임을 개설하시겠습니까?</div>
+      </Modal>
+      {isProvedModal ? (
+        <ProvedComponent
+          setCloseModal={setIsProvedModal}
+          setMakingTeam={setShowingMakingTeamModal}
+        />
+      ) : null}
+      {showingMakingTeamModal ? <MakingTeam /> : null}
+    </>
   );
 };
 
