116110b9a6462cd105e652502be3ccd51fb35d2b
Jeongeun-Choi <cje3080@gmail.com>
2020-10-25 03:15:07 +0900

    [Update] Geocode 구하는 함수 생성 - 위도와 경도를 구하는 함수를 추가해줬다.

--- components/map/KakaoMap.jsx
+++ components/map/KakaoMap.jsx
@@ -1,9 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import Geocode from 'react-geocode';
+import { GEOCODE_API } from '../../api';
 
-const { kakao } = window;
+const KakaoMap = ({ location }) => {
+  const [lat, setLat] = useState(0);
+  const [lng, setLng] = useState(0);
 
-const KakaoMap = ({ lat, lng }) => {
   useEffect(() => {
+    Geocode.setApiKey(GEOCODE_API);
+    Geocode.setLanguage('kr');
+
+    Geocode.fromAddress(location).then(
+      response => {
+        setLat(response.results[0].geometry.location.lat);
+        setLng(response.results[0].geometry.location.lng);
+      },
+      error => {
+        console.error(error);
+      }
+    );
+
     const container = document.getElementById('map');
     const options = {
       center: new kakao.maps.LatLng(lat, lng),
