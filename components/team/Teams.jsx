650d00391a4704311ac99d49d280070cd1235df3
Jeongeun-Choi <cje3080@gmail.com>
2020-10-31 15:30:44 +0900

    [Add] 팀 관리 페이지의 Group Item 생성

--- components/team/Teams.jsx
+++ components/team/Teams.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import EmptyTeams from './EmptyTeams';
 import styled from '@emotion/styled';
-import { basicTeamStyle } from '../../public/style';
+import Team from './Team';
 
 const TeamsContainer = styled.main`
   width: 100%;
@@ -9,54 +8,25 @@ const TeamsContainer = styled.main`
   display: flex;
   flex-direction: column;
   align-items: center;
-`;
 
-const MyTeam = styled.div`
-  width: 95%;
-  height: 52px;
-  display: flex;
-  justify-content: space-around;
-  margin-bottom: 11px;
-
-  .team-image {
-    ${basicTeamStyle}
-    width: 20%;
-    min-width: 74.74px;
-  }
-  .team-info {
-    ${basicTeamStyle}
-    width: 60%;
-    min-width: 247.44px;
-    display: flex;
-    flex-direction: column;
-    font-size: 11px;
+  & ul {
+    list-style: none;
   }
 `;
 
-const Teams = ({ tab }) => {
+const Teams = ({ tab, groups }) => {
   return (
-    // <EmptyTeams pageTab={tab} userName="최정은" />
     <TeamsContainer>
-      <MyTeam>
-        <section className="team-image">
-          <div>대충 이미지</div>
-        </section>
-        <section className="team-info">
-          <div>카테고리</div>
-          <div>모임명</div>
-          <div>시/구</div>
-        </section>
-      </MyTeam>
-      <MyTeam>
-        <section className="team-image">
-          <div>대충 이미지</div>
-        </section>
-        <section className="team-info">
-          <div>카테고리</div>
-          <div>모임명</div>
-          <div>시/구</div>
-        </section>
-      </MyTeam>
+      <ul>
+        {groups.map(group => (
+          <Team
+            key={group.id}
+            groupId={group.Group.id}
+            groupName={group.Group.name}
+            data={group}
+          />
+        ))}
+      </ul>
     </TeamsContainer>
   );
 };
