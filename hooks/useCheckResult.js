263d3797da58818c8d9f0a846b4f516bce305dcb
geonwoomun <38036598+geonwoomun@users.noreply.github.com>
2020-11-01 19:20:11 +0900

    feat: 마지막에 확인하는 화면을 보여주는 customhook

---
+++ hooks/useCheckResult.js
@@ -0,0 +1,92 @@
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
+import styled from '@emotion/styled';
+
+const Screen = styled.div`
+  width: 100%;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: rgba(125, 125, 125, 0.7);
+
+  & .button_container {
+    width: 30%;
+    height: 20vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border-radius: 5px;
+    background-color: white;
+
+    & .content_wrapper {
+      width: 70%;
+      padding: 1rem;
+      border-bottom: 1px solid grey;
+      margin-bottom: 1rem;
+      text-align: center;
+
+      & .screen_title {
+        font-weight: bold;
+        font-size: 1rem;
+      }
+
+      & .screen_content {
+        font-size: 1em;
+      }
+    }
+
+    & button {
+      color: #1c32ff;
+      background: white;
+      border: none;
+      cursor: pointer;
+    }
+  }
+`;
+
+const useCheckResult = ({ title, content, pushUrl, onClick }) => {
+  const router = useRouter();
+  const [isShow, setIsShow] = useState(false);
+
+  const toggleShow = () => {
+    setIsShow((prev) => !prev);
+  };
+
+  const clickHandler = () => {
+    pushUrl && router.push(pushUrl);
+    onClick && onClick();
+  };
+
+  const closeScreen = () => {
+    setIsShow(false);
+  };
+
+  const CheckScreen = () => {
+    return (
+      <>
+        {isShow && (
+          <Screen onClick={closeScreen}>
+            <div className='button_container'>
+              <div className='content_wrapper'>
+                {title && <div className='screen_title'>{title}</div>}
+                {content && <div className='screen_content'>{content}</div>}
+              </div>
+
+              <button onClick={clickHandler}>확인</button>
+            </div>
+          </Screen>
+        )}
+      </>
+    );
+  };
+
+  return [toggleShow, CheckScreen];
+};
+
+export default useCheckResult;
