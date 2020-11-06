1fd8760ebfe9847e91b08dd6a29ed2edb627138e
blair-lee <dakyung.dev@gmail.com>
2020-11-06 15:30:39 +0900

    [Style] css

---
+++ components/setting/ProvedComponent.jsx
@@ -0,0 +1,154 @@
+import React, { useState, useCallback } from 'react';
+import {
+  Modal,
+  ModalHeader,
+  BasicInput,
+  ShortInput,
+  modalFooter 
+} from '../../public/style';
+import styled from '@emotion/styled';
+import { Select } from 'antd';
+import { LeftOutlined } from '@ant-design/icons';
+
+const ProvedContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  & main {
+    & p {
+      margin-top: 15%;
+      font-family: 'Nanum Gothic', sans-serif;
+      font-style: regular;
+      font-size: 12px;
+      line-height: 14px;
+      text-align: center;
+    }
+    & span {
+      //color: #6055CD;
+      color: #6055CD;
+    }
+    & .input-item {
+      font-family: 'Nanum Gothic', sans-serif;
+      font-style: regular;
+      font-size: 12px;
+      color: #868686;
+    }
+    & .form-item {
+      margin-bottom: 5%;
+      width: 75%;
+    }
+    & .form-item-phone {
+      display: flex;
+    }
+    & button{
+      width: 133%;
+      margin-top: 3%;
+      background-color: white;
+      border: 1px solid #6055CD;
+      border-radius: 2px;
+      font-family: 'Nanum Gothic', sans-serif;
+      font-style: regular;
+      font-size: 12px;
+      //color: #6055CD;
+      color: #6055CD;
+    }
+    & .try-again{
+      margin-top: 20%;
+      color: #868686;
+      font-family: 'Nanum Gothic', sans-serif;
+      font-style: regular;
+      font-size: 12px;
+      text-align: center;
+    }
+  }
+`;
+
+const ModalFooter = styled.button`
+  ${modalFooter}
+  color: #ffffff;
+  // background-color: #6055CD;
+  // border: 1px solid #6055CD;
+  background-color: #6055CD;
+  border: 1px solid #6055CD;
+  font-weight: bold;
+  position: fixed;
+  bottom: 0;
+`;
+
+const ProvedComponent = ({ setCloseModal, setMakingTeam }) => {
+  const [mobile, setMobile] = useState('');
+  const moblieChange = useCallback(value => {
+    setMobile(value);
+  }, []);
+
+  const proveUser = useCallback(e => {
+    e.preventDefault();
+    setCloseModal(prev => !prev);
+    setMakingTeam(prev => !prev);
+  }, []);
+
+  const closeModal = useCallback(() => {
+    setCloseModal(prev => !prev);
+  }, []);
+
+  return (
+    <Modal>
+      <ProvedContainer>
+        <ModalHeader>
+          <LeftOutlined onClick={closeModal} />
+          <h3>#6055CD</h3>
+        </ModalHeader>
+        <main>
+          <p>
+            안전하고 즐거운 모임 운영을 위해
+            <br />
+            #6055CD을 해주세요.
+          </p>
+          <div className="form-content">
+            <div className="form-item">
+              <div className="input-item">
+                이름<span> *</span>
+              </div>
+              <BasicInput />
+            </div>
+            <div className="form-item">
+              <div className="input-item"> 
+                통신사<span> *</span>
+              </div>
+              <Select defaultValue="SKT" onChange={moblieChange}>
+                <Select.Option value="SKT">SKT</Select.Option>
+                <Select.Option value="KT">KT</Select.Option>
+                <Select.Option value="LG">LG</Select.Option>
+              </Select>
+            </div>
+            {/*<div className="form-item form-item-phone"> 기존코드*/}
+            <div className="form-item">
+              <div className="input-item">
+                휴대전화번호<span> *</span>
+              </div>
+              {/*<ShortInput />*/}
+              <BasicInput />
+              
+              <div className="get-provedNum">
+                <button>인증번호 전송</button>
+              </div>
+
+            </div>
+            <div className="form-item">
+              <div className="input-item">
+                인증번호<span> *</span>
+              </div>
+              <BasicInput />
+            </div>
+          </div>
+          <div className="try-again">인증번호가 도착하지 않는다면? <b>재전송</b></div>
+        </main>
+        <ModalFooter onClick={proveUser}>완료</ModalFooter>
+      </ProvedContainer>
+    </Modal>
+  );
+};
+
+export default ProvedComponent;
--- components/setting/Withdrawal.jsx
+++ components/setting/Withdrawal.jsx
@@ -10,16 +10,21 @@ const Container = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
+  font-family: 'Nanum Gothic', sans-serif;
+
   & img {
+    margin-top: 10%;
     width: 30vw;
-    max-width: 300px;
-    min-width: 200px;
+    max-width: 200px;
+    min-width: 150px;
   }
 
   & .text_wrapper {
+    margin-top: 10%;
     width: 100%;
     max-width: 500px;
-    padding: 0 4rem;
+    padding: 0 2.5rem;
+    font-size: 12px;
   }
 
   & button {
@@ -50,29 +55,29 @@ const Withdrawal = () => {
   });
 
   return (
-    <Container>
-      <img src='/images/logo.png' alt='로고'></img>
-      <div className='text_wrapper'>
-        <div>
-          <strong>그동안 모두의 모임을 이용해주셔서 감사합니다.</strong>
-        </div>
-        <div>아래의 내용을 잘 확인하시고, 회원탈퇴를 진행해주세요.</div>
+  <Container>
+    <img src='/images/logo.png' alt='로고'></img>
+    <div className='text_wrapper'>
+      <div>
+        <b>그동안 모두의 모임을 이용해주셔서 감사합니다.</b>
+      </div>
+      <div>아래의 내용을 확인 후, 회원탈퇴를 진행해주세요.</div>
 
-        <div>
-          모두의 모임을 <strong>탈퇴 하시면</strong>
-          <br />
-          회원정보, 가입하신 팀, 그룹 피드, 대화내용이 초기화 되며{' '}
-          <strong>복구하실 수 없습니다.</strong>
-        </div>
-        <div>
-          정말로 탈퇴를 원하신다면
-          <br /> 회원 탈퇴 버튼을 눌러주세요.
-        </div>
+      <div>
+        <br></br>모두의 모임을 <b>탈퇴 하시면</b>
+        <br />
+        회원정보, 가입하신 팀, 그룹 피드, 대화내용이<br></br>초기화되며{' '}
+        <b>복구하실 수 없습니다.</b>
       </div>
 
-      <button onClick={toggleShow}>회원 탈퇴</button>
-      <CheckScreen />
-    </Container>
+      <div>
+        <br></br>정말로 탈퇴를 원하신다면
+        <br /> 회원 탈퇴 버튼을 눌러주세요.
+      </div>
+    </div>
+    <button onClick={toggleShow}>회원 탈퇴</button>
+    <CheckScreen />
+      </Container>
   );
 };
 
