d8d0dc40d9c5c43ba820692ea55ea0abf427a059
blair-lee <dakyung.dev@gmail.com>
2020-11-14 21:54:44 +0900

    [Style] css

--- components/post/QnaList.jsx
+++ components/post/QnaList.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import QnaItem from './QnaItem';
+import styled from '@emotion/styled';
+
+const NoQna = styled.div`
+  text-align: center;
+  font-size: 0.8rem;
+  margin-top: 0.5rem;
+  color: #868686;
+`
 
 const QnaList = ({ setQnas, groupId, qnas, isMyGroup }) => {
-  if (qnas.length === 0) return <div>qna가 없습니다.</div>;
+  if (qnas.length === 0) return <NoQna>등록된 Q&A가 없습니다.</NoQna>;
 
   return (
     <ul>
